6f34bb8f5a96f9d118c4f93724f214bcdace2c16
Ahmet Narmanlı <narmanli.ahmet@gmail.com>
2020-11-23 09:14:14 +0300

    New method for bist-30 initiated.

--- python/diffPred/DPNN_train.py
+++ python/diffPred/DPNN_train.py
@@ -3,22 +3,8 @@ import numpy as np
 import cv2 as cv
 import sklearn as sk
 
-df = pd.read_csv("data.csv")
-df = df.iloc[3270-2050:-1, 5:21:1]
-ndf = df.to_numpy(dtype=float)
+df = pd.read_csv("bist-30.csv",delimiter=';')
+dfi = df.iloc[:-40, 1]
+ndf = dfi.to_numpy(dtype=float)
 np.nan_to_num(ndf)
 ndif = np.diff(ndf, axis=0)
-minf = np.min(ndif)
-ranf = (np.max(ndif)-np.min(ndif))
-ndif = (255*(ndif - minf))/ranf
-imgD = ndif.astype(np.uint8)
-cv.imshow("denem", imgD)
-cv.waitKey()
-
-
-#for i in np.arange(0,2048,16):
- #   cv.imwrite("data1"+str(i/16+1)+".jpg", )
-
-
-
-
