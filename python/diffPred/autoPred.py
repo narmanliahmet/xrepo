7e035168ca670423fb696ad29fbdb57a36d72e37
Ahmet Narmanlı <narmanli.ahmet@gmail.com>
2020-11-28 13:18:38 +0300

    1 day prediction optimized weights are saved.

--- python/diffPred/autoPred.py
+++ python/diffPred/autoPred.py
@@ -7,25 +7,19 @@ import numpy as np
 df = pd.read_csv('bist-301.csv', delimiter=';')
 df = df.iloc[1:, 1]
 df = pd.Series.to_numpy(df)
-
 dfn = df.reshape(-1, 1)
 # fit a model
-model = Sequential()
-model.add(Dense(300, input_dim=1, activation='relu'))
-model.add(Dense(1))
-# compile a model
-model.compile(loss='mse', optimizer='adam')
-# fit a model
-model.fit(dfn[0:-130].reshape(-1, 1), dfn[1:-129].reshape(-1, 1), epochs=400, batch_size=200, verbose=0)
-# predict last 1
-res = np.zeros(10)
-for n in range(10):
-    mse = model.predict(dfn[-129+n].reshape(-1, 1), verbose=0)
-    res[n] = mse
+res = 0
+while np.abs(res-dfn[-129]) > 7:
+    model = Sequential()
+    model.add(Dense(300, input_dim=1, activation='relu'))
+    model.add(Dense(1))
+    # compile a model
+    model.compile(loss='mse', optimizer='adam')
+    # fit a model
+    model.fit(dfn[0:-130].reshape(-1, 1), dfn[1:-129].reshape(-1, 1), epochs=400, batch_size=200, verbose=0)
+    # predict last 1
 
-plt.figure()
-plt.plot(range(10), res, label='prediction')
-plt.plot(range(10), dfn[-128:-118], label='real')
-plt.title("Prediction Comparison")
-plt.legend()
-plt.show()
+    res = model.predict(dfn[-130].reshape(-1, 1), verbose=0)
+    print(res)
+    print(dfn[-129])
