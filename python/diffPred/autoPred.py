c4698b485f9885c8d63e92b8dec8eab71b584355
Ahmet Narmanlı <narmanli.ahmet@gmail.com>
2020-11-28 12:41:24 +0300

    Succesive prediction network is trained over optimization.

--- python/diffPred/autoPred.py
+++ python/diffPred/autoPred.py
@@ -7,6 +7,7 @@ import numpy as np
 df = pd.read_csv('bist-301.csv', delimiter=';')
 df = df.iloc[1:, 1]
 df = pd.Series.to_numpy(df)
+
 dfn = df.reshape(-1, 1)
 # fit a model
 model = Sequential()
@@ -15,15 +16,16 @@ model.add(Dense(1))
 # compile a model
 model.compile(loss='mse', optimizer='adam')
 # fit a model
-model.fit(dfn[0:-130].reshape(-1, 1), dfn[1:-129].reshape(-1, 1), epochs=900, batch_size=200, verbose=0)
+model.fit(dfn[0:-130].reshape(-1, 1), dfn[1:-129].reshape(-1, 1), epochs=400, batch_size=200, verbose=0)
 # predict last 1
-err = np.zeros(10)
+res = np.zeros(10)
 for n in range(10):
     mse = model.predict(dfn[-129+n].reshape(-1, 1), verbose=0)
-    err[n] = np.abs(mse-dfn[-128+n])
+    res[n] = mse
 
 plt.figure()
-plt.plot(range(10), err, label='error')
-plt.title("Error of predictions")
+plt.plot(range(10), res, label='prediction')
+plt.plot(range(10), dfn[-128:-118], label='real')
+plt.title("Prediction Comparison")
 plt.legend()
 plt.show()
