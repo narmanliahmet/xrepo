544077e76440d1847b25cafc80ba59224e9c298a
Ahmet Narmanlı <narmanli.ahmet@gmail.com>
2021-01-26 02:22:56 +0300

    Stream trials for start button are made.

--- python/Qt/VoiceGrab/utils.py
+++ python/Qt/VoiceGrab/utils.py
@@ -1,15 +1,21 @@
 import numpy as np
 import sounddevice as sd
+import asyncio
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import QThread, pyqtSignal
 
 
 class Utillities:
+
     def __init__(self, *args, **kwargs):
         super(Utillities, self).__init__(*args, **kwargs)
         self.out = np.empty(shape=4, dtype=np.ndarray)
         self.fout = np.empty(shape=4, dtype=np.ndarray)
+        self.buffer = np.ndarray
         self.fs = 48000
         self.dur = 8
         self.N = self.fs * self.dur
+        self.ap = AudioProcess()
         sd.default.samplerate = self.fs
         sd.default.channels = 1
 
@@ -70,10 +76,12 @@ class Utillities:
         print("Text 4")
 
     def start(self):
-        print("Start")
+        self.ap.flag = True
+        self.ap.streamAudio(self.buffer)
 
     def stop(self):
         print("Stop")
+        self.ap.flag = False
 
     def lamp1(self):
         print("Lamp 1")
@@ -94,11 +102,30 @@ def message():
 
 class AudioProcess:
 
+    def __init__(self, *args, **kwargs):
+        super(AudioProcess, self).__init__(*args, **kwargs)
+        self.fs = 48000
+        self.dur = 1
+        self.N = self.fs * self.dur
+        self.flag = True
+        sd.default.samplerate = self.fs
+        sd.default.channels = 1
+
     def corr(self, x, y):
         return np.corrcoef(x, y)
 
-    def streamAudio(self):
-        pass
+    def streamAudio(self, buffer):
+        print("Stream Started")
+        try:
+            sd.default.device = 2, 4
+        except:
+            sd.default.device = 1, 3
+        while self.flag:
+            buffer = sd.rec(int(self.N))
+            QTest.qWait(1000)
+            print(buffer)
+        print("Stream Finished")
+        print(buffer)
 
     def grabClient(self, audio):
         pass
