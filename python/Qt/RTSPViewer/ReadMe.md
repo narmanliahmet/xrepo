f1aff6a3dc5e074d6c065a0e74957c75abd2f9f3
Ahmet Narmanlı <narmanli.ahmet@gmail.com>
2021-01-31 16:42:16 +0300

    Prototype Rtsp viewer finalized for use.

---
+++ python/Qt/RTSPViewer/ReadMe.md
@@ -0,0 +1,18 @@
+# RTSP Viewer 0.0.1
+
+## Dependency Python Packages
+- PyQt5
+- python-vlc
+
+## Usage
+- Use rtsp link text edit to input your rtsp link.
+- Push the load button.
+- Control the stream over play/pause button in the midddle
+  and stream line slider.
+- Use stop button to reset the stream
+- Use snap button to take a snap shot.
+- Use volume slider to adjust volume over stream
+
+Bugs to be fixed.
+- Slider movement over play mode gives a delay to play again.
+- Sometimes slider move over play mode make stream pause.
--- python/Qt/RTSPViewer/rtspwindow.py
+++ python/Qt/RTSPViewer/rtspwindow.py
@@ -1,15 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QTextEdit, QComboBox, QLineEdit, QFrame, \
-    QSlider
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QTextEdit, QLineEdit, QFrame, \
+    QSlider, QFileDialog, QMessageBox
+from PyQt5 import QtTest
 import vlc
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 
 class MainWindow(QMainWindow):
+    # Worker for Thread
+
+    class Worker(QObject):
+        pass
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        # Threads
+
+        self.thrd = QThread()
+        self.worker = MainWindow.Worker()
+        self.worker.moveToThread(self.thrd)
+        self.thrd.started.connect(self.update)
+
+        # Popups
+
+        self.dial = QFileDialog()
+        # Flags
+
+        self.flag = False
+        self.sflag = True
+        self.lflag = False
+        self.volume = 30
 
         # Main Style
         self.setWindowTitle("RTSPViewer-0.0.1")
@@ -28,7 +49,9 @@ class MainWindow(QMainWindow):
 
         # Load Text Edit and Fps, Resolution Configuration
 
-        self.tedit = QTextEdit("RTSP Link")
+        self.tedit = QTextEdit()
+        self.tedit.setPlaceholderText("RTSP Link")
+        self.tedit.setAcceptRichText(False)
         self.tedit.setStyleSheet("background-color: rgb(250,250,250);"
                                  "border: 2px solid gray;"
                                  "border-radius: 7px;"
@@ -42,15 +65,12 @@ class MainWindow(QMainWindow):
                                 "color: white;}"
                                 "QPushButton:pressed {background-color: rgb(100,100,100);}")
 
-        self.res = QComboBox()
-        self.res.addItems(["1080p", "760p", "480p", "360p", "240p"])
+        self.res = QLabel("Res(N/A)")
         self.res.setStyleSheet("background-color: rgb(80,80,80);"
                                "border: 2px solid white;"
                                "border-radius: 7px;"
                                "font: 20px;"
-                               "color: white;"
-                               "selection-background-color: rgb(100,100,100);"
-                               "selection-color: rgb(200, 200, 200);")
+                               "color: white;")
 
         self.fpsl = QLabel("FPS")
         self.fpsl.setStyleSheet("background-color: rgb(80,80,80);"
@@ -61,6 +81,7 @@ class MainWindow(QMainWindow):
                                 )
 
         self.fpst = QLineEdit("30")
+        self.fpst.setReadOnly(True)
         self.fpst.setAlignment(Qt.AlignCenter)
         self.fpst.setStyleSheet("background-color: rgb(250,250,250);"
                                 "border: 2px solid gray;"
@@ -71,8 +92,6 @@ class MainWindow(QMainWindow):
         # Display Screen
         self.instance = vlc.Instance()
         self.mp = self.instance.media_player_new()
-        self.media = self.instance.media_new("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov")
-        self.mp.set_media(self.media)
 
         self.stl = QLabel(self)
         self.stl.setStyleSheet("background-color: #b91;"
@@ -155,14 +174,7 @@ class MainWindow(QMainWindow):
                               "border - radius: 4"
                               "px;"
                               "} ")
-        self.bw = QPushButton("<<")
-        self.bw.setStyleSheet("QPushButton {background-color: rgb(80,80,80);"
-                              "border: 4px solid white;"
-                              "border-radius: 15px;"
-                              "font: 40px;"
-                              "font-weight: bold;"
-                              "color: white;}"
-                              "QPushButton:pressed {background-color: rgb(100,100,100);}")
+
         self.pp = QPushButton(">")
         self.pp.setStyleSheet("QPushButton {background-color: rgb(80,80,80);"
                               "border: 4px solid white;"
@@ -171,14 +183,7 @@ class MainWindow(QMainWindow):
                               "font-weight: bold;"
                               "color: white;}"
                               "QPushButton:pressed {background-color: rgb(100,100,100);}")
-        self.fw = QPushButton(">>")
-        self.fw.setStyleSheet("QPushButton {background-color: rgb(80,80,80);"
-                              "border: 4px solid white;"
-                              "border-radius: 15px;"
-                              "font: 40px;"
-                              "font-weight: bold;"
-                              "color: white;}"
-                              "QPushButton:pressed {background-color: rgb(100,100,100);}")
+
         self.stop = QPushButton("Stop")
         self.stop.setStyleSheet("QPushButton {background-color: rgb(80,80,80);"
                                 "border: 4px solid white;"
@@ -203,7 +208,7 @@ class MainWindow(QMainWindow):
         self.vol = QSlider(Qt.Vertical)
         self.vol.setTickPosition(QSlider.TicksRight)
         self.vol.setRange(0, 100)
-        self.vol.setValue(30)
+        self.vol.setValue(self.volume)
         self.vol.setStyleSheet("QSlider::groove:vertical {"
                                "border: 1px solid #bbb;"
                                "background: white;"
@@ -284,15 +289,6 @@ class MainWindow(QMainWindow):
                                 "font: 20px;"
                                 "color: white;"
                                 )
-        self.pace = QLabel("No Media")
-        self.pace.setAlignment(Qt.AlignCenter)
-        self.pace.setStyleSheet("background-color: rgb(80,80,80);"
-                                "border: 4px solid white;"
-                                "border-radius: 15px;"
-                                "font: 30px;"
-                                "font-weight: bold;"
-                                "color: white;"
-                                )
 
         # Widget Resize and Location
 
@@ -320,20 +316,14 @@ class MainWindow(QMainWindow):
         self.vol.move(35, 950)
         self.stop.resize(200, 100)
         self.stop.move(250, 950)
-        self.bw.resize(200, 100)
-        self.bw.move(550, 950)
         self.pp.resize(200, 100)
         self.pp.move(850, 950)
-        self.fw.resize(200, 100)
-        self.fw.move(1150, 950)
         self.ss.resize(200, 100)
         self.ss.move(1450, 950)
         self.telp.resize(100, 30)
         self.telp.move(10, 860)
         self.trem.resize(100, 30)
         self.trem.move(1810, 860)
-        self.pace.resize(200, 100)
-        self.pace.move(1675, 950)
 
         # Widget Parenting
 
@@ -348,14 +338,158 @@ class MainWindow(QMainWindow):
         self.volL.setParent(self)
         self.vol.setParent(self)
         self.stop.setParent(self)
-        self.bw.setParent(self)
         self.pp.setParent(self)
-        self.fw.setParent(self)
         self.ss.setParent(self)
         self.telp.setParent(self)
         self.trem.setParent(self)
-        self.pace.setParent(self)
+
+        # Widget binding
+
+        self.load.clicked.connect(self.loadrtsp)
+        self.pp.clicked.connect(self.play)
+        self.stop.clicked.connect(self.sstop)
+        self.ss.clicked.connect(self.snap)
+        self.vol.valueChanged.connect(self.setvol)
+        self.ps.sliderMoved.connect(self.settime)
+        self.ps.sliderPressed.connect(self.settime)
+        self.ps.sliderReleased.connect(self.slrls)
+
+    # Sub functions
+
+    def loadrtsp(self):
+        self.media = self.instance.media_new(self.tedit.toPlainText())
+        self.mp.set_media(self.media)
         self.mp.play()
+        QtTest.QTest.qWait(3000)
+        length = self.mp.get_length()
+        self.ps.setRange(0, length)
+        length = int(length // 1e+3)
+
+        if length == -1 or not self.mp.is_playing():
+            mes = QMessageBox(self)
+            mes.setText("No Appropriate Media")
+            mes.setIcon(QMessageBox.Warning)
+            mes.setWindowTitle("Warning")
+            mes.exec_()
+        else:
+            self.flag = True
+            self.sflag = False
+            self.lflag = True
+            self.pp.setText("||")
+            if (length // 60 // 60) > 9:
+                hour = str(length // 60 // 60)
+            else:
+                hour = "0" + str(length // 60 // 60)
+
+            if (length // 60) > 9:
+                minu = str(length // 60)
+            else:
+                minu = "0" + str(length // 60)
+
+            if (length % 60) > 9:
+                sec = str(length % 60)
+            else:
+                sec = "0" + str(length % 60)
+
+            self.trem.setText(hour + ":" + minu + ":" + sec)
+
+        QtTest.QTest.qWait(3000)
+        reso = self.mp.video_get_size()
+        self.res.setText(str(reso[0]) + "p")
+        self.fpst.setText(str(self.mp.get_fps()))
+        self.thrd.start()
+
+    def play(self):
+        self.sflag = False
+        if self.lflag:
+            if self.flag:
+                self.flag = False
+                self.pp.setText(">")
+                self.mp.pause()
+            elif not self.flag:
+                self.flag = True
+                self.pp.setText("||")
+                self.mp.play()
+
+    def sstop(self):
+        self.sflag = True
+        self.flag = False
+        self.pp.setText(">")
+        self.mp.stop()
+
+    def snap(self):
+        if self.flag:
+            self.flag = False
+            self.pp.setText(">")
+            self.mp.pause()
+        if not self.sflag:
+            filename = self.dial.getSaveFileName(self, "Take Snapshot", "", ".jpeg")
+            aspect = self.mp.video_get_size()
+            self.mp.video_take_snapshot(0, filename[0] + filename[1], aspect[0], aspect[1])
+
+    def setvol(self):
+        self.volume = self.vol.value()
+        self.mp.audio_set_volume(self.volume)
+
+    def settime(self):
+        if self.lflag:
+            self.mp.pause()
+            self.mp.set_time(self.ps.value())
+
+    def slrls(self):
+        if self.flag:
+            QtTest.QTest.qWait(500)
+            self.mp.play()
+
+    # Thread slots
+
+    @pyqtSlot()
+    def update(self):
+        while True:
+            if not self.sflag:
+                self.ps.setValue(self.mp.get_time())
+                elap = self.mp.get_time()
+                rem = self.mp.get_length() - self.mp.get_time()
+                elap = int(elap // 1e+3)
+                rem = int(rem // 1e+3)
+                if (elap // 60 // 60) > 9:
+                    houre = str(elap // 60 // 60)
+                else:
+                    houre = "0" + str(elap // 60 // 60)
+
+                if (elap // 60) > 9:
+                    minue = str(elap // 60)
+                else:
+                    minue = "0" + str(elap // 60)
+
+                if (elap % 60) > 9:
+                    sece = str(elap % 60)
+                else:
+                    sece = "0" + str(elap % 60)
+
+                if (rem // 60 // 60) > 9:
+                    hourr = str(rem // 60 // 60)
+                else:
+                    hourr = "0" + str(rem // 60 // 60)
+
+                if (elap // 60) > 9:
+                    minur = str(rem // 60)
+                else:
+                    minur = "0" + str(rem // 60)
+
+                if (rem % 60) > 9:
+                    secr = str(rem % 60)
+                else:
+                    secr = "0" + str(rem % 60)
+
+                self.telp.setText(houre + ":" + minue + ":" + sece)
+                self.trem.setText(hourr + ":" + minur + ":" + secr)
+                QtTest.QTest.qWait(100)
+            else:
+                self.telp.setText("00:00:00")
+                self.trem.setText("00:00:00")
+                self.ps.setValue(0)
+                QtTest.QTest.qWait(100)
 
 
 if __name__ == "__main__":
